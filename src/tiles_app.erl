9c4b1ef52d268b3e1a7be7eab91ec4223a9c1526
Alexander Griffith <griffitaj@gmail.com>
2017-11-02 23:47:19 -0400

    made port and index files external

--- src/tiles_app.erl
+++ src/tiles_app.erl
@@ -2,18 +2,23 @@
 -behaviour(application).
 -export([start/2, stop/1,version/0]).
 
+
 start(_StartType, _StartArgs) ->
     lager:debug("Starting App", []),
+    {ok,Index}=application:get_env(tiles,index),
+    {ok,Port}=application:get_env(tiles,port),
+
+    io:format("~p~n",[Port]),
     Dispatch = cowboy_router:compile(
                  [
                   {'_', [
-                         {"/", cowboy_static, {priv_file,tiles,"testing.html"}},
+                         {"/", cowboy_static, {priv_file,tiles,Index}},
                          {"/assets/[...]", cowboy_static, {priv_dir,tiles,"assets"}},
                          {"/api/v0/match",tiles_match_handler,[]},
                          {"/websocket", tiles_handler, []}
                         ]}
                  ]),
-    {ok, _} = cowboy:start_clear(http, [{port, 10000}],#{
+    {ok, _} = cowboy:start_clear(http, [{port, Port}],#{
                                          env => #{dispatch => Dispatch},
                                          stream_handlers =>
                                              [cowboy_compress_h, cowboy_stream_h]
